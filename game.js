f8566b5e1d84587dfb6b9da84908e7c4828c2f55
soyabean909 <kimberly_sierra@students.edu.sg>
2025-03-24 09:10:57 +0000

    Tilesprite

--- game.js
+++ game.js
@@ -7,28 +7,37 @@ const config = {
         default: 'arcade',
         arcade: {
             gravity: { y: 300 },
-            debug: false
-        }
-    },
     scene: {
         preload: preload,
         create: create,
         update: update
     }
-};
+},
 
-const game = new Phaser.Game(config);
+const :  new Phaser.Game(config),
 
 let player;
+let ground;
+let clouds;
 
-function preload() {
-    
-}
+function preload(){
+    this.load.image("ground", "assets/ground.png");
+    this.load.image("cloud", "assets/cloud.png"); 
+this.load.spritesheet("dino","assets/dino-idle.png",{frameWidth: 88, framHeight:94});}
 
 function create() {
-    
+    this.add.image(100,300,"ground")
+    this.add.image(300,130,"cloud")
+    this.add.image(500,130,"cloud")
+    this.add.image(100,130,"cloud")
+    this.physics.add.sprite(200,200,"dino").setOrigin(0);
+    this.clouds=this.add.group();
+    this.clouds=this.clouds.addMultiple(
+        [this.add.image(300,130,"cloud"),
+        this.add.image(500,130,"cloud"),
+        this.add.image(100,130,"cloud")]
+    );
 }
-
 function update() {
 
 }
